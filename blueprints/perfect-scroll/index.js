d40e474a368f4f8633e57fb34e5a3f84631ac8cf
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2015-08-25 19:22:07 +0600

    Small bug fix

--- blueprints/perfect-scroll/index.js
+++ blueprints/perfect-scroll/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  description: 'Component wrapper for perfect-scrollbar'
+  description: 'Component wrapper for perfect-scrollbar',
 
   afterInstall: function(options) {
     return this.addBowerPackageToProject('perfect-scrollbar');
