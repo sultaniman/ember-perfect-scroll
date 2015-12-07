59b72a06d63917b8ed567d8f29b682046c00b4ac
Falk Pauser <falk.pauser@gmail.com>
2015-12-07 10:54:50 +0100

    Added normalizeEntity to blueprint

--- blueprints/perfect-scroll/index.js
+++ blueprints/perfect-scroll/index.js
@@ -1,6 +1,12 @@
 module.exports = {
   description: 'Component wrapper for perfect-scrollbar',
 
+  normalizeEntityName: function() {
+		// this prevents an error when the entityName is
+		// not specified (since that doesn't actually matter
+		// to us
+	},
+
   afterInstall: function(options) {
     return this.addBowerPackageToProject('perfect-scrollbar');
   }
