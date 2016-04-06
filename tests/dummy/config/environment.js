1594530d6ad39b283ff62ca53fae883ee9aa3c5d
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-04-06 18:10:38 +0600

    update(dummy app)

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -16,7 +16,6 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      hinting: false
     }
   };
 
