9201b1856523f6eeeb3f35db370d133b654ed6f1
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2016-08-30 20:58:41 +0200

    update(blueprint) generate ember-perfect-scroll blueprint

---
+++ blueprints/ember-perfect-scroll/index.js
@@ -0,0 +1,13 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'Component wrapper for perfect-scrollbar',
+
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter to us
+  },
+
+  afterInstall: function(options) {
+    return this.addBowerPackageToProject('perfect-scrollbar');
+  }
+};
